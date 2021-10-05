10ec1022a6345262776e43af175d97aae606b9cc
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-10-05 23:26:30 +0330

    commit-7

--- Every-Taste/src/app/app-routing.module.ts
+++ Every-Taste/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RecipesComponent} from "./recipes/recipes.component";
+import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo: '/recipes', pathMatch:'full' },
+  {path:'recipes',component:RecipesComponent},
+  {path:'shopping-list',component:ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
