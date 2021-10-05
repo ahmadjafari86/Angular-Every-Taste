b11c1abbde4173da842618eb2bbc8242ccd91846
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-10-06 01:07:51 +0330

    commit-9

--- Every-Taste/src/app/recipes/recipe.service.ts
+++ Every-Taste/src/app/recipes/recipe.service.ts
@@ -12,12 +12,14 @@ export class RecipeService{
       'https://upload.wikimedia.org/wikipedia/commons/4/48/Ugali_%26_Sukuma_Wiki.jpg',
       [new Ingredient('Meat',1),new Ingredient('French fries',10)]),
     new Recipe('The best recipe','This is simply the best',
-      'https://lh3.googleusercontent.com/proxy/C1p0jZMfWnY9vPBujsZIEMkxc8nLC9FIRDefTylyXhyVhUbJ_azt28QMqE4Pw-' +
-      'MgDMFLzDXScBc0cq7zkZGnX2_SQcVSF3FWiQCbJtaA9MED4d0uAo0bZxNXLo71Fxb0ww3cdJkuLo8',
+      'https://www.publicdomainpictures.net/pictures/290000/nahled/hamburger-lunch.jpg',
       [new Ingredient('Buns',5),new Ingredient('Onion',2)])
   ];
   getRecipes(){
     return this.recipes.slice();
+  }
+  getRecipe(index:number){
+    return this.recipes[index];
   }
   addIngeToShoList(ingredients:Ingredient[]){
      this.shoppingListService.addIngredients(ingredients);
